fa3d5de1e495295028616088842d32a8c169459a
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2021-08-19 08:04:08 +0000

    feat: add transform function

--- src/index.ts
+++ src/index.ts
@@ -48,11 +48,11 @@ export function parseFileAsync(filePath: string): PromiseLike<Program> {
   });
 }
 
-export function generate(ast: Program, callback: (err: Error | null, output: string) => void): void;
+export function generate(ast: Program, callback: (err: Error | null, output: string | null) => void): void;
 export function generate(
   ast: Program,
   options: GeneratorOptions,
-  callback: (err: Error | null, output: string) => void,
+  callback: (err: Error | null, output: string | null) => void,
 ): void;
 export function generate(
   ast: Program,
@@ -70,7 +70,98 @@ export function generate(
 export function generateAsync(ast: Program, options?: GeneratorOptions): PromiseLike<string> {
   return new Promise<string>((resolve, reject) => {
     generate(ast, options ?? {}, (err, output) => {
-      if (err) {
+      if (err || !output) {
+        reject(err);
+      } else {
+        resolve(output);
+      }
+    });
+  });
+}
+
+export type TransformOptions = GeneratorOptions;
+
+export function transform(code: string, callback: (err: Error | null, output: string | null) => void): void;
+export function transform(
+  code: string,
+  options: TransformOptions,
+  callback: (err: Error | null, output: string | null) => void,
+): void;
+export function transform(
+  code: string,
+  optionsOrCallback: TransformOptions | ((err: Error | null, output: string | null) => void),
+  callback?: (err: Error | null, output: string | null) => void,
+): void {
+  let options = {};
+  if (typeof optionsOrCallback === "function") {
+    callback = optionsOrCallback;
+    options = {};
+  }
+  parse(code, (err, ast) => {
+    if (err || !ast) {
+      callback?.(err, null);
+    } else {
+      generate(ast, options, (err, output) => {
+        if (err) {
+          callback?.(err, null);
+        } else {
+          callback?.(null, output);
+        }
+      });
+    }
+  });
+}
+
+export function transformAsync(code: string, options?: GeneratorOptions): PromiseLike<string> {
+  return new Promise<string>((resolve, reject) => {
+    transform(code, options ?? {}, (err, output) => {
+      if (err || !output) {
+        reject(err);
+      } else {
+        resolve(output);
+      }
+    });
+  });
+}
+
+export function transformFile(filePath: string, callback: (err: Error | null, output: string | null) => void): void;
+export function transformFile(
+  filePath: string,
+  options: TransformOptions,
+  callback: (err: Error | null, output: string | null) => void,
+): void;
+export function transformFile(
+  filePath: string,
+  optionsOrCallback: TransformOptions | ((err: Error | null, output: string | null) => void),
+  callback?: (err: Error | null, output: string | null) => void,
+): void {
+  let options = {};
+  if (typeof optionsOrCallback === "function") {
+    callback = optionsOrCallback;
+    options = {};
+  }
+  if (typeof callback === "undefined") {
+    throw new Error("callback is required");
+  }
+  readFile(filePath, "utf8", (err, code) => {
+    if (err) {
+      callback?.(err, null);
+    } else {
+      transform(code, options, (err, output) => {
+        if (err) {
+          callback?.(err, null);
+        } else {
+          callback?.(null, output);
+        }
+      });
+    }
+  });
+}
+
+export function transformFileAsync(filePath: string, options?: TransformOptions): PromiseLike<string> {
+  return new Promise<string>((resolve, reject) => {
+    transformFile(filePath, options ?? {}, (err, output) => {
+      if (err || !output) {
         reject(err);
       } else {
         resolve(output);
