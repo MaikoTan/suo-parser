a4889f7a5d8886a948340c21b37813a671049d85
Maiko Tan <maiko.tan.coding@gmail.com>
2024-12-06 11:42:29 +0800

    refactor: convert TypeScript classes to Rust structs and enums

--- src/utils/location.rs
+++ src/utils/location.rs
@@ -1,27 +1,54 @@
-// export class Position {
-//   line: number;
-//   column: number;
+use std::fmt::{Display, Error, Formatter};
 
-//   constructor(line: number, col: number) {
-//     this.line = line;
-//     this.column = col;
-//   }
+pub struct Position {
+  pub line: usize,
+  pub column: usize,
+}
 
-//   toString() {
-//     return `${this.line}:${this.column}`;
-//   }
-// }
+impl Position {
+  pub fn new(line: usize, column: usize) -> Self {
+    Self { line, column }
+  }
+}
 
-// export class SourceLocation {
-//   start: Position;
-//   end: Position;
+impl Display for Position {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(f, "{}:{}", self.line, self.column)
+  }
+}
 
-//   constructor(start: Position, end: Position) {
-//     this.start = start;
-//     this.end = end;
-//   }
+pub struct SourceLocation {
+  pub start: Position,
+  pub end: Position,
+}
 
-//   toString() {
-//     return `${this.start.line}:${this.start.column}`;
-//   }
-// }
+impl SourceLocation {
+  pub fn new(start: Position, end: Position) -> Self {
+    Self { start, end }
+  }
+}
+
+impl Display for SourceLocation {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(f, "{},{}", self.start, self.end)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_position() {
+    let pos = Position::new(1, 2);
+    assert_eq!(pos.to_string(), "1:2");
+  }
+
+  #[test]
+  fn test_source_location() {
+    let start = Position::new(1, 2);
+    let end = Position::new(3, 4);
+    let loc = SourceLocation::new(start, end);
+    assert_eq!(loc.to_string(), "1:2,3:4");
+  }
+}
